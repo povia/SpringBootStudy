4ed05f06f03c5246c805d85e7c72f2ca6311f205
povia <tjddn020@naver.com>
2021-01-25 17:58:08 +0900

    sql resource 외부로 분리, MultiFile 처리 추가

--- board/src/main/java/board/BoardApplication.java
+++ board/src/main/java/board/BoardApplication.java
@@ -2,9 +2,10 @@ package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+
 // 스프링 부트의 핵심 Annotation
-@SpringBootApplication
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class BoardApplication {
 
 	public static void main(String[] args) {
--- board/src/main/java/board/configuration/WebMvcConfiguration.java
+++ board/src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,9 +1,18 @@
 package board.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import board.interceptor.LoggerInterceptor;
+
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
+
 
 public class WebMvcConfiguration implements WebMvcConfigurer{
 
@@ -11,5 +20,32 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggerInterceptor());
 	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver(){
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		// Encoding을 UTF-8으로 맞춤
+		commonsMultipartResolver.setDefaultEncoding("UTF-8");
+		// 최대 5MB까지 허용
+		commonsMultipartResolver.setMaxUploadSizePerFile(5*1024*1024);
+		return commonsMultipartResolver;
+	}
 
+	// Spring Boot 2.1.x 버전 아래에서 한글 처리를 위한 인코딩 필터 설정 시작
+	/*
+	@Bean
+	public Filter characterEncodingFilter(){
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+
+		return characterEncodingFilter;
+	}
+
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter(){
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+	*/
+	// 끝
 }
